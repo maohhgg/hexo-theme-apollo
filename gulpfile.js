0331c09d272f7bf857bbda57f9963707281d4a3f
maohhgg <maohhgg@gmail.com>
2019-12-04 09:05:22 +0800

    update css for latest highlight

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
+sass.compiler = require('node-sass');
+
 // 一次性编译 Sass 
 gulp.task('sass', function() {
     return gulp.src('./source/scss/*.scss')
@@ -9,9 +11,8 @@ gulp.task('sass', function() {
         .pipe(autoprefixer())
         .pipe(gulp.dest('./source/css'));
 });
-
 // 实时编译
-gulp.task('default', ['sass'], function() {
+gulp.task('sass:watch', function () {
     gulp.watch('./source/scss/_partial/*.scss', ['sass']);
     gulp.watch('./source/scss/*.scss', ['sass']);
 });
